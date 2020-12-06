c94f8c9a8885769b5d9704df5196b81c0a61ed8f
Andy Krause <andykrause19@gmail.com>
2020-12-05 20:32:38 -0800

    Add channel preserve test

---
+++ trips/channel_preserve/load.R
@@ -0,0 +1,16 @@
+
+## Set libraries
+library(tidyverse)
+library(sf)
+library(leaflet)
+
+  suppressMessages(imagessf <- sf::st_read(file.path(getwd(), 'trips', 'channel_preserve', 'data', 'images.shp')) %>%
+                   sf::st_transform(4269) )
+
+  trip_df <- read.csv(file.path(getwd(), 'data', 'created', 'test_trip.csv'))
+  imagessf$URL <- trip_df$WebPage
+  imagessf <- imagessf %>%
+    mutate(label = paste(" <a href = ", WebPage, "/>Photo</a>")) 
+
+  save.image(file = file.path(getwd(), 'trips', 'channel_preserve', 'data', 'mapdata.RData'))  
+  
