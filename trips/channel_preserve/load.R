cdc58f31cc40f3452cc399268d4d4bbc625c3474
Andy Krause <andykrause19@gmail.com>
2020-12-05 20:44:36 -0800

    Update to channel preserve

--- trips/channel_preserve/load.R
+++ trips/channel_preserve/load.R
@@ -6,11 +6,14 @@ library(leaflet)
 
   suppressMessages(imagessf <- sf::st_read(file.path(getwd(), 'trips', 'channel_preserve', 'data', 'images.shp')) %>%
                    sf::st_transform(4269) )
+  suppressMessages(hikessf <- sf::st_read(file.path(getwd(), 'trips', 'channel_preserve', 'data', 'hikes.shp')) %>%
+                     sf::st_transform(4269) )
+  
 
   trip_df <- read.csv(file.path(getwd(), 'data', 'created', 'test_trip.csv'))
   imagessf$URL <- trip_df$WebPage
   imagessf <- imagessf %>%
-    mutate(label = paste(" <a href = ", WebPage, "/>Photo</a>")) 
+    mutate(label = paste(" <a href = ", URL, "/>Photo</a>")) 
 
   save.image(file = file.path(getwd(), 'trips', 'channel_preserve', 'data', 'mapdata.RData'))  
   
