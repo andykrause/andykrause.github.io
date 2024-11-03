692cb33724f97c00ce42069bf303c8f08721de41
Andy Krause <andy@andykrause.com>
2024-11-02 20:45:11 -0700

    Updates to Running Maps
    * Larger plots
    * Separate analysis file
    * Delete unnecessary fields

---
+++ running_maps_prep.R
@@ -0,0 +1,53 @@
+## Load libraries
+library(sf)
+library(geosphere)
+library(tidyverse)
+library(basemaps)
+library(maps)
+library(tmap)
+
+## Source Custom Functions
+source(file.path(getwd(), "functions/mapping_functions.R"))
+
+## Set Paths
+
+data_path <- '~/dropbox/andy/data/where/america'
+image_path <- 'x'
+output_path <- '~/dropbox/andy/data/projects/running_maps/'
+
+## Load Running Shapefile
+running_sf <- sf::st_read(file.path(data_path, 'running.shp'),
+                          quiet=TRUE) %>%
+  sf::st_transform(4269) 
+
+## Split into trail/road 
+
+trailruns_sf <- running_sf %>%
+  dplyr::filter(!is.na(type)) %>%
+  dplyr::filter(type == 'trail') 
+
+roadruns_sf <- running_sf %>%
+  dplyr::filter(!is.na(type)) %>%
+  dplyr::filter(type != 'trail')
+
+
+sf::st_write(trailruns_sf, file.path(output_path, 'trail_runs.shp'), append = FALSE)
+sf::st_write(roadruns_sf, file.path(output_path, 'road_runs.shp'), append = FALSE)
+
+# trail_coords <- sf::st_coordinates(trailruns_sf)
+# 
+# trail_cuts <- floor(nrow(trail_coords)/1000) + 1
+# 
+# a <- 1
+# b <- 1
+# 
+# for(i in 1:trail_cuts){
+#   b <- trail_coords[i*1000,4]-1
+#   fil <- paste0('trail_', i, '.kml')
+#   sf::st_write(trailruns_sf$geometry[a:b], paste0('~/dropbox/andy/data/where/gaia/', fil),
+#                append=FALSE)
+#   a <- b+1
+# }
+# 
+# 
+# head(h)
